2f708ebc5d82b47992a662c7353cef7d864c71c2
Your Name <younes.fennane@kellogg.northwestern.edu>
2021-04-13 16:38:56 -0500

    Final draft Noober

--- noober.js
+++ noober.js
@@ -12,6 +12,19 @@ document.addEventListener('DOMContentLoaded', async function() {
 
   // 🔥 YOUR CODE GOES HERE 🔥
   // Write the recipe (algorithm) in the comments. Then, write the code.
+  // extract and store basic information from the ride Object
+  let firstName = ride.passengerDetails.first
+  let lastName = ride.passengerDetails.last
+  let phoneNumber = ride.passengerDetails.phoneNumber
+  let pickupLocation = ride.pickupLocation
+  let dropoffLocation = ride.dropoffLocation
   
+  // Display rider + trip information on Console
+console.log (`Passenger's name & Phone number : ${firstName}, ${lastName} - ${phoneNumber}`)
+console.log (`Trip itinerary : from : ${pickupLocation.address}, ${pickupLocation.city}, ${pickupLocation.state}, ${pickupLocation.zip} to : ${dropoffLocation.address}, ${dropoffLocation.city}, ${dropoffLocation.state}, ${dropoffLocation.zip}  `)
+
+// Display level of service on console
+if (ride.purpleRequested == true) {console.log (`level of service : Noober Purple`)} else if (ride.numberOfPassengers > 3 && ride.purpleRequested == false) {console.log(`level of service : Noober XL`)} else {console.log (`level of service : Noober X`)}
+
   // 🔥 YOUR CODE ENDS HERE 🔥
 })
